ec0fa15cfcef015830a9b4af3536d8c7c9bc7a82
Saffan Md <67745780+saffan19@users.noreply.github.com>
2021-11-09 21:12:50 +0530

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,27 @@
+print("abcd")
+print("ab\ncd")
+a="hello"
+b=" world"
+print(a+b)
+print(int("80")+1)
+print(round(-2.2))
+print(min(2,5,3))
+print(min(2,5,3,-10,22))
+
+prime=[3,45,6,7]
+print(prime[2])
+print(prime[2:])
+print(prime[:2])
+
+a=[
+    ['m','n','0'],
+    ['p','q','t']
+]
+print(a[0])
+print(a[0][1])
+
+
+t=(1,2,3)
+print(t)
+tt=(1,2,'abc')
+print(tt)
